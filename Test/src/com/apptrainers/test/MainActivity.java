efc716661dfa07c3e375cc9943b2ff854ef67911
AppTrainers-Hanan <hanan.nimer@apptrainers.com>
2013-07-14 10:44:45 +0300

    MainActivity-testing2

--- Test/src/com/apptrainers/test/MainActivity.java
+++ Test/src/com/apptrainers/test/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Log.e("Hanan","Repo for testing synchrosizing");
+		Log.e("Hanan","Repo for testing synchrosizing2");
 	}
 
 	@Override
